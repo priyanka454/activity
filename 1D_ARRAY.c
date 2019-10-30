c06f8315ae78ea78c962ba92516dd485aa8c8668
priyanka454 <56225187+priyanka454@users.noreply.github.com>
2019-10-30 18:28:14 +0530

    Add files via upload

---
+++ 1D_ARRAY.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main() {
+  int maxlen=10;                 //#defin MAX_SIZE 10
+  int arr[10];               //int arr[MAX_SIZE]
+
+  srand(time(NULL));
+  for(int i=0;i<maxlen;i++)
+    arr[i]=rand()%100;
+
+  int sum=0;
+  for(int i=0;i<maxlen;i++)
+  {
+    printf("%d\n",arr[i]);
+    sum+=arr[i];
+  }
+  printf("Sum of array elements is %d\n",sum);
+
+  return 0;
+}
