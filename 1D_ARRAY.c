0765a2fc9872c5ce8d0c147dcdf1ded9a6084d95
priyanka454 <56225187+priyanka454@users.noreply.github.com>
2019-10-30 22:57:46 +0530

    Update 1D_ARRAY.c

--- 1D_ARRAY.c
+++ 1D_ARRAY.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
 int main() {
-  int maxlen;                 //#defin MAX_SIZE 10
+  int maxlen=10,j;                 //#defin MAX_SIZE 10
   int arr[10];               //int arr[MAX_SIZE]
-
+print("")
   srand(time(NULL));
   for(int i=0;i<maxlen;i++)
     arr[i]=rand()%100;
